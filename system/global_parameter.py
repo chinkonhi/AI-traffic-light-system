d16ade14b42c10764f5e5a521199055cd8e1c424
Chin KonHi <luffychen1202@gmail.com>
2019-11-06 11:36:37 +0900

    add global_parameter.py

---
+++ system/global_parameter.py
@@ -0,0 +1,12 @@
+#使用定数放置行人速度,长,宽
+PERS_SPEED = 4
+PERS_HEIGHT = 10
+PERS_WIDTH = 20
+
+#使用定数放置车辆速度,车辆长,宽
+CAR_HEIGHT = 80
+CAR_WIDTH = 40
+
+CAR_ACCELERATE = 3 # 加速度
+CAR_INITIAL = 0 # 初速度
+CAR_SPEED = 15 # 最高速
